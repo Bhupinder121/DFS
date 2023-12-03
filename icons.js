411e98cd2e002ba6ed71de6a3c3c0404d269ac91
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2023-12-03 14:42:20 +0530

    alot happened

---
+++ icons.js
@@ -0,0 +1,13 @@
+class Icon{
+    constructor(image, x, y, sizex, sizey, connectx, connecty){
+        this.img = image;
+        this.pos = createVector(x, y);
+        this.sizeX = sizex;
+        this.sizeY = sizey;
+        this.connectX = connectx;
+        this.connectY = connecty;
+
+    }
+
+    
+}
