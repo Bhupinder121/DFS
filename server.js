8e41d195108bfddfdce9d901e55d3d06cbca6c79
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2023-12-08 12:14:54 +0530

    monitor added

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const status = require("express-status-monitor");
 const PORT = 300;
 
+app.use(status());
 app.use(express.static(__dirname + "/public"));
+
+
 
 app.get('/', (req, res)=>{
     console.log("new connection");
