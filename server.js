c611fd99c7b62d078b0fa6bb3af27269e2637980
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2023-12-12 19:36:03 +0530

    redemption

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
 const status = require("express-status-monitor");
+const ejs = require("ejs");
 const PORT = 300;
 
+app.set('view-engine', 'ejs');
 app.use(status());
+
 app.use(express.static(__dirname + "/public"));
 
 
 
 app.get('/', (req, res)=>{
     console.log("new connection");
-    res.sendFile(__dirname + "/public/index.html");
+    res.render(__dirname + "/public/index.ejs");
 });
 
 app.listen(PORT, ()=>{
