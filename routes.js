895c538b9dc939f17f6e0431894746fa4d5305aa
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2023-12-23 12:59:10 +0530

    chucked data
    sending hopefully chucked data need to test data from client to server

--- routes.js
+++ routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const Router = express.Router();
 const multer  = require('multer')
-const upload = multer()
+// const multer = multer()
+
+const storage = multer.memoryStorage(); // Store files in memory as buffers
+const upload = multer({ storage: storage });
 Router.use(express.static(__dirname + "/public"));
 
 
@@ -16,8 +19,12 @@ Router.get("/home", (req, res)=>{
     res.render(__dirname+"/public/home.ejs")
 });
 
-Router.post("/upload", upload.single('file'), (req, res)=>{
-    res.send(req.file);
-});
+Router.post('/upload', upload.single('file'), (req, res) => {
+    const fileBuffer = req.file.buffer;
+    const textData = fileBuffer.toString('utf-8');
+    console.log('Text data:', textData);
+
+    res.json({'message': "recevied data from client"});
+  });
 
 module.exports = Router;
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const ejs = require("ejs");
 const routes = require('./routes.js');
 const PORT = 300;
+
 app.set('view-engine', 'ejs');
 app.use("/", routes)
-
 app.use(express.static(__dirname + "/public"));
 
 
